6904e8668da3b87523b680616e6e1be5a5258f72
Sanders Li <65951961+sanderslhc@users.noreply.github.com>
2021-07-20 11:14:57 +0800

    Update consensus analysis1.R

--- code/consensus analysis1.R
+++ code/consensus analysis1.R
@@ -24,16 +24,27 @@ names(datExpr0) = case$ID;
 rownames(datExpr0) = names(case)[c(2:7)];
 dim(datExpr0)
 
-#remove missing values
+#Check that all genes and samples have sufficiently low numbers of missing values.
 gsg = goodSamplesGenes(datExpr0, verbose = 3);
 gsg$allOK
+
+if (!gsg$allOK)
+{
+  # Optionally, print the gene and sample names that were removed:
+  if (sum(!gsg$goodGenes)>0) 
+    printFlush(paste("Removing genes:", paste(names(datExpr0)[!gsg$goodGenes], collapse = ", ")));
+  if (sum(!gsg$goodSamples)>0) 
+    printFlush(paste("Removing samples:", paste(rownames(datExpr0)[!gsg$goodSamples], collapse = ", ")));
+  # Remove the offending genes and samples from the data:
+  datExpr0 = datExpr0[gsg$goodSamples, gsg$goodGenes]
+}
 
 sampleTree = hclust(dist(datExpr0), method = "average");
 # Plot the sample tree: Open a graphic output window of size 12 by 9 inches
 # The user should change the dimensions if the window is too large or too small.
 sizeGrWindow(12,9)
 
-#pdf(file = "Plots/sampleClustering.pdf", width = 12, height = 9);
+#Sample clustering to detect outliers
 par(cex = 0.6);
 par(mar = c(0,4,2,0))
 plot(sampleTree, main = "Sample clustering to detect outliers", sub="", xlab="", cex.lab = 1.5, 
@@ -51,7 +62,7 @@ datExpr = datExpr0
 nGenes = ncol(datExpr)
 nSamples = nrow(datExpr)
 
-#Read the traits imformation
+#Read the traits file
 traitData = read.csv("ClinicalTraits-case.csv");
 dim(traitData)
 names(traitData)
