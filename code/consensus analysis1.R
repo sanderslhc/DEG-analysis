d19c80d22125d9bd922121daad3146188cb34b29
Sanders Li <65951961+sanderslhc@users.noreply.github.com>
2021-07-20 10:21:53 +0800

    Update consensus analysis1.R

--- code/consensus analysis1.R
+++ code/consensus analysis1.R
@@ -1,77 +1,32 @@
-#=====================================================================================
-#
-#  Code chunk 1
-#
-#=====================================================================================
-
-
 # Display the current working directory
 rm(list = ls())
 options(stringsAsFactors = F)
 getwd();
 # If necessary, change the path below to the directory where the data files are stored. 
 # "." means current directory. On Windows use a forward slash / instead of the usual \.
-workingDir = "E:/R/workspace/WGCNA/consensus module/JJ";
+workingDir = "E:/R/workspace/WGCNA/consensus module/ICV";
 setwd(workingDir); 
 
-# Load the WGCNA package
+# Load the WGCNA and stringr package
 library(WGCNA);
+library(stringr);
 # The following setting is important, do not omit.
 options(stringsAsFactors = FALSE);
 #Read in the female liver data 
-case= read.csv("JJ-case.csv");
+case= read.csv("ICV-case.csv");
 # Take a quick look at what is in the data set:
 dim(case);
 names(case);
 
-
-#=====================================================================================
-#
-#  Code chunk 2
-#
-#=====================================================================================
-
-
-datExpr0 = as.data.frame(t(case[, c(2:20)]));
+#convert data
+datExpr0 = as.data.frame(t(case[, c(2:7)]));
 names(datExpr0) = case$ID;
-rownames(datExpr0) = names(case)[c(2:20)];
+rownames(datExpr0) = names(case)[c(2:7)];
 dim(datExpr0)
-#=====================================================================================
-#
-#  Code chunk 3
-#
-#=====================================================================================
 
-
+#remove missing values
 gsg = goodSamplesGenes(datExpr0, verbose = 3);
 gsg$allOK
-
-
-#=====================================================================================
-#
-#  Code chunk 4
-#
-#=====================================================================================
-
-
-if (!gsg$allOK)
-{
-  # Optionally, print the gene and sample names that were removed:
-  if (sum(!gsg$goodGenes)>0) 
-    printFlush(paste("Removing genes:", paste(names(datExpr0)[!gsg$goodGenes], collapse = ", ")));
-  if (sum(!gsg$goodSamples)>0) 
-    printFlush(paste("Removing samples:", paste(rownames(datExpr0)[!gsg$goodSamples], collapse = ", ")));
-  # Remove the offending genes and samples from the data:
-  datExpr0 = datExpr0[gsg$goodSamples, gsg$goodGenes]
-}
-
-
-#=====================================================================================
-#
-#  Code chunk 5
-#
-#=====================================================================================
-
 
 sampleTree = hclust(dist(datExpr0), method = "average");
 # Plot the sample tree: Open a graphic output window of size 12 by 9 inches
@@ -83,14 +38,6 @@ par(cex = 0.6);
 par(mar = c(0,4,2,0))
 plot(sampleTree, main = "Sample clustering to detect outliers", sub="", xlab="", cex.lab = 1.5, 
      cex.axis = 1.5, cex.main = 2)
-
-
-#=====================================================================================
-#
-#  Code chunk 6
-#
-#=====================================================================================
-
 
 # Plot a line to show the cut
 abline(h = 15, col = "red");
@@ -104,14 +51,7 @@ datExpr = datExpr0
 nGenes = ncol(datExpr)
 nSamples = nrow(datExpr)
 
-
-#=====================================================================================
-#
-#  Code chunk 7
-#
-#=====================================================================================
-
-
+#Read the traits imformation
 traitData = read.csv("ClinicalTraits-case.csv");
 dim(traitData)
 names(traitData)
@@ -130,69 +70,14 @@ rownames(dataTraits) = allTraits[traitRows, 1];
 
 collectGarbage();
 
-
-
-#=====================================================================================
-#
-#  Code chunk 8
-#
-#=====================================================================================
-
-
 # Re-cluster samples
 sampleTree2 = hclust(dist(datExpr), method = "average")
 # Convert traits to a color representation: white means low, red means high, grey means missing entry
-traitColors = numbers2colors(datTraits, signed = FALSE);
+traitColors = numbers2colors(datExpr, signed = FALSE);
 # Plot the sample dendrogram and the colors underneath.
 plotDendroAndColors(sampleTree2, traitColors,
-                    groupLabels = names(datTraits), 
+                    groupLabels = names(dataTraits), 
                     main = "Sample dendrogram and trait heatmap")
-
-
-#=====================================================================================
-#
-#  Code chunk 9
-#
-#=====================================================================================
-
-
-save(datExpr, datTraits, file = "CASE-01-dataInput.RData")
-
-#=====================================================================================
-#
-#  Code chunk 1
-#
-#=====================================================================================
-
-
-# Display the current working directory
-getwd();
-# If necessary, change the path below to the directory where the data files are stored. 
-# "." means current directory. On Windows use a forward slash / instead of the usual \.
-workingDir = ".";
-setwd(workingDir); 
-# Load the WGCNA package
-library(WGCNA)
-# The following setting is important, do not omit.
-options(stringsAsFactors = FALSE);
-# Allow multi-threading within WGCNA. This helps speed up certain calculations.
-# At present this call is necessary for the code to work.
-# Any error here may be ignored but you may want to update WGCNA if you see one.
-# Caution: skip this line if you run RStudio or other third-party R environments. 
-# See note above.
-enableWGCNAThreads()
-# Load the data saved in the first part
-lnames = load(file = "CASE-01-dataInput.RData");
-#The variable lnames contains the names of loaded variables.
-lnames
-
-
-#=====================================================================================
-#
-#  Code chunk 2
-#
-#=====================================================================================
-
 
 # Choose a set of soft-thresholding powers
 powers = c(c(1:10), seq(from = 12, to=20, by=2))
@@ -219,15 +104,7 @@ plot(sft$fitIndices[,1], sft$fitIndices[,5],
      main = paste("Mean connectivity"))
 text(sft$fitIndices[,1], sft$fitIndices[,5], labels=powers, cex=cex1,col="red")
 
-
-#=====================================================================================
-#
-#  Code chunk 3
-#
-#=====================================================================================
-
-
-
+#Build a co-expression matrix
 net = blockwiseModules(datExpr, power = 9,
                        TOMType = "unsigned", minModuleSize = 100,
                        reassignThreshold = 0, mergeCutHeight = 0.25,
@@ -235,14 +112,6 @@ net = blockwiseModules(datExpr, power = 9,
                        saveTOMs = TRUE,
                        saveTOMFileBase = "CASETOM", 
                        verbose = 3)
-
-
-#=====================================================================================
-#
-#  Code chunk 4
-#
-#=====================================================================================
-
 
 # open a graphics window
 sizeGrWindow(12, 9)
@@ -253,15 +122,6 @@ plotDendroAndColors(net$dendrograms[[1]], mergedColors[net$blockGenes[[1]]],
                     "Module colors",
                     dendroLabels = FALSE, hang = 0.03,
                     addGuide = TRUE, guideHang = 0.05)
-
-
-#=====================================================================================
-#
-#  Code chunk 5
-#
-#=====================================================================================
-
-
 moduleLabels = net$colors
 moduleColors = labels2colors(net$colors)
 MEs = net$MEs;
@@ -269,7 +129,7 @@ geneTree = net$dendrograms[[1]];
 save(MEs, moduleLabels, moduleColors, geneTree, 
      file = "CASE-02-networkConstruction-auto.RData")
 
-
+#Same-character association analysis
 design=model.matrix(~0+ traitData$tissue)
 colnames(design)=levels(as.factor(traitData$tissue))
 moduleColors <- labels2colors(net$colors)
@@ -293,6 +153,8 @@ labeledHeatmap(Matrix = moduleTraitCor,
                cex.text = 0.5,
                zlim = c(-1,1),
                main = paste("Module-trait relationships"))
+
+#Inter-module correlation
 MEs = net$MEs
 MEs_col = MEs
 colnames(MEs_col) = paste0("ME", labels2colors(
@@ -303,10 +165,11 @@ plotEigengeneNetworks(MEs_col, "Eigengene adjacency heatmap",
                       marHeatmap = c(3,4,2,2), plotDendrograms = T, 
                       xLabelsAngle = 90)
 
-
+#save the case-analysis result
 lnames = load(file = "CASE-02-networkConstruction-auto.RData");
 lnames
 
+#extract genes
 module='black'
 probes=colnames(datExpr)
 inModule=(moduleColors==module)
@@ -314,7 +177,7 @@ modProbes=probes[inModule]
 head(modProbes)
 write.csv(modProbes, file = 'E:/R/results/module-gene/case-control/JJ/black.csv')
 
-#GO/KEGG·ÖÎö
+#GO/KEGG analysis
 setwd('E:/R/results/module-gene/case-control/JJ/')
 library(clusterProfiler)
 library(org.Bt.eg.db)
@@ -329,3 +192,4 @@ write.csv(ego,"E:/R/results/WGCNA/JJ-GO.csv",row.names = F)
 
 kk <- enrichKEGG(eg$ENTREZID,pvalueCutoff = 0.05,qvalueCutoff = 0.05,organism = "bta")
 write.csv(kk,"E:/R/results/WGCNA/JJ-KEGG.csv",row.names = F) 
+
