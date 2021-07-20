9f57a0b2772a32aef7f67077a40e27843136a259
Sanders Li <65951961+sanderslhc@users.noreply.github.com>
2021-07-20 16:17:42 +0800

    Update consensus analysis2.R

--- code/consensus analysis2.R
+++ code/consensus analysis2.R
@@ -1,38 +1,24 @@
-#=====================================================================================
-#
-#  Code chunk 1
-#
-#=====================================================================================
-
 rm(list = ls())
 options(stringsAsFactors = F)
 # Display the current working directory
 getwd();
 # If necessary, change the path below to the directory where the data files are stored. 
 # "." means current directory. On Windows use a forward slash / instead of the usual \.
-workingDir = "E:/R/workspace/WGCNA/consensus module/JJ";
+  workingDir = "E:/R/workspace/WGCNA/consensus module/ICV";
 setwd(workingDir); 
 # Load the package
 library(WGCNA);
 # The following setting is important, do not omit.
 options(stringsAsFactors = FALSE);
 #Read in the female liver data set
-case = read.csv("≥¶µ¿-case.csv");
+case = read.csv("ËÇ†ÈÅì-case.csv");
 # Read in the male liver data set
-control = read.csv("≥¶µ¿-control.csv");
+control = read.csv("ËÇ†ÈÅì-control.csv");
 # Take a quick look at what is in the data sets (caution, longish output):
 dim(control)
 names(control)
 dim(case)
 names(case)
-
-
-#=====================================================================================
-#
-#  Code chunk 2
-#
-#=====================================================================================
-
 
 # We work with two sets:
 nSets = 2;
@@ -51,25 +37,9 @@ rownames(multiExpr[[2]]$data) = names(control)[c(2:7)];
 # Check that the data has the correct format for many functions operating on multiple sets:
 exprSize = checkSets(multiExpr)
 
-
-#=====================================================================================
-#
-#  Code chunk 3
-#
-#=====================================================================================
-
-
 # Check that all genes and samples have sufficiently low numbers of missing values.
 gsg = goodSamplesGenesMS(multiExpr, verbose = 3);
 gsg$allOK
-
-
-#=====================================================================================
-#
-#  Code chunk 4
-#
-#=====================================================================================
-
 
 if (!gsg$allOK)
 {
@@ -89,50 +59,23 @@ if (!gsg$allOK)
   exprSize = checkSets(multiExpr)
 }
 
-
-#=====================================================================================
-#
-#  Code chunk 5
-#
-#=====================================================================================
-
-
 sampleTrees = list()
 for (set in 1:nSets)
 {
   sampleTrees[[set]] = hclust(dist(multiExpr[[set]]$data), method = "average")
 }
 
-
-#=====================================================================================
-#
-#  Code chunk 6
-#
-#=====================================================================================
-
-
-pdf(file = "Plots/SampleClustering.pdf", width = 12, height = 12);
 par(mfrow=c(2,1))
 par(mar = c(0, 4, 2, 0))
 for (set in 1:nSets)
   plot(sampleTrees[[set]], main = paste("Sample clustering on all genes in", setLabels[set]),
        xlab="", sub="", cex = 0.7);
-dev.off();
-
-
-#=====================================================================================
-#
-#  Code chunk 7
-#
-#=====================================================================================
-
 
 # Choose the "base" cut height for the female data set
 baseHeight = 16
 # Adjust the cut height for the male data set for the number of samples
 cutHeights = c(16, 16*exprSize$nSamples[2]/exprSize$nSamples[1]);
 # Re-plot the dendrograms including the cut lines
-pdf(file = "Plots/SampleClustering.pdf", width = 12, height = 12);
 par(mfrow=c(2,1))
 par(mar = c(0, 4, 2, 0))
 for (set in 1:nSets)
@@ -141,37 +84,8 @@ for (set in 1:nSets)
        xlab="", sub="", cex = 0.7);
   abline(h=cutHeights[set], col = "red");
 }
-dev.off();
 
-
-#=====================================================================================
-#
-#  Code chunk 8
-#
-#=====================================================================================
-
-
-for (set in 1:nSets)
-{
-  # Find clusters cut by the line
-  labels = cutreeStatic(sampleTrees[[set]], cutHeight = cutHeights[set])
-  # Keep the largest one (labeled by the number 1)
-  keep = (labels==1)
-  multiExpr[[set]]$data = multiExpr[[set]]$data[keep, ]
-}
-collectGarbage();
-# Check the size of the leftover data
-exprSize = checkSets(multiExpr)
-exprSize
-
-
-#=====================================================================================
-#
-#  Code chunk 9
-#
-#=====================================================================================
-
-
+#read the traits file
 traitData = read.csv("ClinicalTraits.csv");
 dim(traitData)
 names(traitData)
@@ -194,53 +108,6 @@ collectGarbage();
 # Define data set dimensions
 nGenes = exprSize$nGenes;
 nSamples = exprSize$nSamples;
-
-
-#=====================================================================================
-#
-#  Code chunk 10
-#
-#=====================================================================================
-save(multiExpr, Traits, nGenes, nSamples, setLabels, shortLabels, exprSize, 
-     file = "Consensus-dataInput.RData");
-
-
-#=====================================================================================
-#
-#  Code chunk 1
-#
-#=====================================================================================
-
-
-# Display the current working directory
-getwd();
-# If necessary, change the path below to the directory where the data files are stored. 
-# "." means current directory. On Windows use a forward slash / instead of the usual \.
-workingDir = ".";
-setwd(workingDir); 
-# Load the WGCNA package
-library(WGCNA)
-# The following setting is important, do not omit.
-options(stringsAsFactors = FALSE);
-# Allow multi-threading within WGCNA. 
-# Caution: skip this line if you run RStudio or other third-party R environments.
-# See note above.
-enableWGCNAThreads()
-
-# Load the data saved in the first part
-lnames = load(file = "Consensus-dataInput.RData");
-# The variable lnames contains the names of loaded variables.
-lnames
-# Get the number of sets in the multiExpr structure.
-nSets = checkSets(multiExpr)$nSets
-
-
-#=====================================================================================
-#
-#  Code chunk 2
-#
-#=====================================================================================
-
 
 # Choose a set of soft-thresholding powers
 powers = c(seq(4,10,by=1), seq(12,20, by=2));
@@ -270,7 +137,6 @@ for (set in 1:nSets)
 }
 # Plot the quantities in the chosen columns vs. the soft thresholding power
 sizeGrWindow(8, 6)
-tiff(file = "E:/◊™¬º◊È∑÷Œˆ-ƒÃ≈£∏±Ω·∫À/¿Ó∫Ò≥œ-Œƒ’¬–ﬁ∏ƒÕº/WGCNA/JJ.tiff", res = 600, width = 4800, height = 4800, compression = "lzw")
 par(mfcol = c(2,2));
 par(mar = c(4.2, 4.2 , 2.2, 0.5))
 cex1 = 0.7;
@@ -296,15 +162,6 @@ for (col in 1:length(plotCols)) for (set in 1:nSets)
   } else
     legend("topright", legend = setLabels, col = colors, pch = 20) ;
 }
-sft
-dev.off();
-
-
-#=====================================================================================
-#
-#  Code chunk 3
-#
-#=====================================================================================
 
 net = blockwiseConsensusModules(
   multiExpr, power = 10, minModuleSize = 100, deepSplit = 2, cex.text = 1,
@@ -312,12 +169,6 @@ net = blockwiseConsensusModules(
   mergeCutHeight = 0.25, numericLabels = TRUE,
   minKMEtoStay = 0,
   saveTOMs = TRUE, verbose = 5)
-#=====================================================================================
-#
-#  Code chunk 4
-#
-#=====================================================================================
-
 
 consMEs = net$multiMEs;
 moduleLabels = net$colors;
@@ -325,67 +176,14 @@ moduleLabels = net$colors;
 moduleColors = labels2colors(moduleLabels)
 consTree = net$dendrograms[[1]]; 
 
-
-#=====================================================================================
-#
-#  Code chunk 5
-#
-#=====================================================================================
-
-
 sizeGrWindow(8,6);
-tiff(file = "E:/◊™¬º◊È∑÷Œˆ-ƒÃ≈£∏±Ω·∫À/¿Ó∫Ò≥œ-Œƒ’¬–ﬁ∏ƒÕº/WGCNA/PB1.tiff", res = 600, width = 4800, height = 4800, compression = "lzw")
 plotDendroAndColors(consTree, moduleColors,
                     "Module colors",
                     dendroLabels = FALSE, hang = 0.03,
                     addGuide = TRUE, guideHang = 0.05,
                     main = "Consensus gene dendrogram and module colors")
 
-dev.off()
-
-
-#=====================================================================================
-#
-#  Code chunk 6
-#
-#=====================================================================================
-save(consMEs, moduleLabels, moduleColors, consTree, file = "Consensus-NetworkConstruction-auto.RData")
-
-#=====================================================================================
-#
-#  Code chunk 1
-#
-#=====================================================================================
-
-
-# Display the current working directory
-getwd();
-# If necessary, change the path below to the directory where the data files are stored. 
-# "." means current directory. On Windows use a forward slash / instead of the usual \.
-workingDir = ".";
-setwd(workingDir); 
-# Load the WGCNA package
-library(WGCNA)
-# The following setting is important, do not omit.
-options(stringsAsFactors = FALSE);
-# Load the data saved in the first part
-lnames = load(file = "Consensus-dataInput.RData");
-
-#The variable lnames contains the names of loaded variables.
-lnames
-# Load the results of network analysis, tutorial part 2.a
-lnames = load(file = "Consensus-NetworkConstruction-auto.RData");
-lnames
-
-
-#=====================================================================================
-#
-#  Code chunk 2
-#
-#=====================================================================================
-
-
-lnames = load("FemaleLiver-02-networkConstruction-auto.RData")
+lnames = load("CASE-networkConstruction-auto.RData")
 lnames
 # Rename variables to avoid conflicts
 femaleLabels = moduleLabels;
@@ -456,7 +254,7 @@ par(mfrow=c(1,1));
 par(cex = 1.0);
 par(mar=c(8, 10.4, 2.7, 1)+0.3);
 # Use function labeledHeatmap to produce the color-coded table with all the trimmings
-tiff(file = "E:/◊™¬º◊È∑÷Œˆ-ƒÃ≈£∏±Ω·∫À/¿Ó∫Ò≥œ-Œƒ’¬–ﬁ∏ƒÕº/WGCNA/Peripheral blood.tiff", res = 600, width = 2800, height = 2000, compression = "lzw")
+tiff(file = "E:/ËΩ¨ÂΩïÁªÑÂàÜÊûê-Â•∂ÁâõÂâØÁªìÊ†∏/ÊùéÂéöËØö-ÊñáÁ´†‰øÆÊîπÂõæ/WGCNA/Peripheral blood.tiff", res = 600, width = 2800, height = 2000, compression = "lzw")
 labeledHeatmap(Matrix = pTable,
                xLabels = paste(" ", consModules),
                yLabels = paste(" ", femModules),
